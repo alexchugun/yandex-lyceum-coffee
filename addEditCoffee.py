faa6d13ca2e7d69809064a3d5821c0c3144383f2
Alexandr Chugunov <alexchugunov@mail.ru>
2024-01-12 21:06:33 +0700

    Task Cappuccino complete

---
+++ addEditCoffee.py
@@ -0,0 +1,66 @@
+import sys
+
+from PyQt5 import uic
+from PyQt5.QtSql import QSqlDatabase, QSqlTableModel
+from PyQt5.QtWidgets import QWidget, QTableView, QApplication, QMainWindow
+
+
+class AddEditCoffee(QMainWindow):
+    def __init__(self, parent, data=None):
+        super().__init__()
+
+        self.index = 0
+        self.data = data
+        self.is_add = True if data is None else False
+
+        self.parent = parent
+
+        self.initUI()
+
+    def initUI(self):
+        uic.loadUi('addEditCoffeeForm.ui', self)
+
+        if self.data is not None:
+            self.set_data()
+
+        self.saveButton.clicked.connect(self.save)
+
+    def set_data(self):
+        self.nameEdit.setText(str(self.data['name']))
+        self.degreeRoastEdit.setText(str(self.data['degree_roast']))
+        self.isGround.setChecked(int(self.data['is_ground']) == 1)
+        self.descriptionEdit.setText(str(self.data['description']))
+        self.priceEdit.setText(str(self.data['price']))
+        self.volumeEdit.setText(str(self.data['volume']))
+
+        self.index = int(self.data['current_index'])
+
+    def save(self):
+        name = self.nameEdit.text()
+        degree_roast = self.degreeRoastEdit.text()
+        is_ground = self.isGround.isChecked()
+        description = self.descriptionEdit.text()
+        price = self.priceEdit.text()
+        volume = self.volumeEdit.text()
+
+        if '' in [name, degree_roast, description, price, volume]:
+            return
+            # тут бы надо вывести окно что есть пустые поля и сделать проверку введенных данных
+
+        r = self.parent.model.record() if self.is_add else self.parent.model.record(self.index)
+        r.setValue('name', name)
+        r.setValue('degree_roast', degree_roast)
+        r.setValue('isGround', 1 if is_ground else 0)
+        r.setValue('description', description)
+        r.setValue('price', price)
+        r.setValue('volume', volume)
+
+        if self.is_add:
+            self.parent.model.insertRecord(-1, r)
+        else:
+            self.parent.model.setRecord(self.index, r)
+            self.parent.model.submitAll()
+
+        self.parent.model.select()
+
+        self.close()
--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from PyQt5 import uic
 from PyQt5.QtSql import QSqlDatabase, QSqlTableModel
 from PyQt5.QtWidgets import QWidget, QTableView, QApplication, QMainWindow
 
+from addEditCoffee import AddEditCoffee
+
 
 class Example(QMainWindow):
     def __init__(self):
@@ -14,15 +16,47 @@ class Example(QMainWindow):
     def initUI(self):
         uic.loadUi('main.ui', self)
 
-        db = QSqlDatabase.addDatabase('QSQLITE')
-        db.setDatabaseName('coffee.sqlite')
-        db.open()
+        self.db = QSqlDatabase.addDatabase('QSQLITE')
+        self.db.setDatabaseName('coffee.sqlite')
+        self.db.open()
 
-        model = QSqlTableModel(self, db)
-        model.setTable('coffee')
-        model.select()
+        self.model = QSqlTableModel(self, self.db)
+        self.model.setTable('coffee')
+        self.model.select()
 
-        self.db_view.setModel(model)
+        self.db_view.setModel(self.model)
+
+        self.addButton.clicked.connect(self.add_data)
+        self.editButton.clicked.connect(self.update_data)
+
+    def add_data(self):
+        self.add_edit_coffee = AddEditCoffee(self)
+        self.add_edit_coffee.show()
+
+    def update_data(self):
+        # print(data)
+        # print(self.db_view.selectionModel().currentIndex().row()) # тут индекс для обновления
+        # selected_index = self.db_view.model().index(self.db_view.selectionModel().currentIndex().row(), 0).data() # тут индекс в самой бд
+
+        current_index = self.db_view.selectionModel().currentIndex().row()
+        name = self.db_view.model().index(current_index, 1).data()
+        degree_roast = self.db_view.model().index(current_index, 2).data()
+        is_ground = self.db_view.model().index(current_index, 3).data()
+        description = self.db_view.model().index(current_index, 4).data()
+        price = self.db_view.model().index(current_index, 5).data()
+        volume = self.db_view.model().index(current_index, 6).data()
+
+        data = {'current_index': current_index,
+                'name': name,
+                'degree_roast': degree_roast,
+                'is_ground': is_ground,
+                'description': description,
+                'price': price,
+                'volume': volume
+                }
+
+        self.add_edit_coffee = AddEditCoffee(self, data=data)
+        self.add_edit_coffee.show()
 
 
 if __name__ == '__main__':
